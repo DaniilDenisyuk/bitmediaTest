6af08aab5241ffb572204eff563be75b7b322fd2
Daniil Denisyuk <ordekonec@gmail.com>
2021-06-15 13:53:42 +0300

    graphics now show zero click if no data provided

--- client-app/src/features/stats/UserStats/index.js
+++ client-app/src/features/stats/UserStats/index.js
@@ -15,22 +15,32 @@ const UserChart = ({ className, title, XYvalues }) => {
   const options = {
     animationEnabled: true,
     axisX: {
-      valueFormatString: "DD MMM",
+      valueFormatString: "DD MMM YY",
       tickLength: 15,
       tickThickness: 0,
+      lineColor: "#CCCCCC",
       labelFontColor: "#CCCCCC",
       labelFontFamily: "mr",
+      scaleBreaks: {
+        type: "straight",
+        autoCalculate: true,
+        collapsibleThreshold: "20%", //change to "50%"
+      },
+      minimum: XYvalues[0].x,
+      maximum: XYvalues[XYvalues.length - 1].x,
     },
     axisY: {
       lineThickness: 0,
       tickLength: 15,
       tickThickness: 0,
+      gridColor: "#CCCCCC",
+      lineColor: "#CCCCCC",
       labelFontColor: "#CCCCCC",
       labelFontFamily: "mr",
     },
     data: [
       {
-        type: "spline",
+        type: "line",
         dataPoints: XYvalues,
       },
     ],
@@ -42,6 +52,11 @@ const UserChart = ({ className, title, XYvalues }) => {
     </div>
   );
 };
+
+const datesAreOnSameDay = (first, second) =>
+  first.getFullYear() === second.getFullYear() &&
+  first.getMonth() === second.getMonth() &&
+  first.getDate() === second.getDate();
 
 const convertDate = (dateObject) => {
   const [, mmm, , yyyy] = dateObject.toDateString().split(" ");
@@ -57,22 +72,39 @@ const UserStats = ({ className }) => {
   const [endDate, setEndDate] = useState(new Date());
   const [filterOpened, setFilterOpened] = useState(false);
 
-  const clickStatsXY = useCallback(
-    () =>
-      userStats.map(({ date, clicks }) => ({
-        x: new Date(date),
-        y: clicks,
-      })),
-    [userStats]
-  );
-  const viewsStatsXY = useCallback(
-    () =>
-      userStats.map(({ date, page_views }) => ({
-        x: new Date(date),
-        y: page_views,
-      })),
-    [userStats]
-  );
+  const clickStatsXY = useCallback(() => {
+    const statsXY = userStats.map(({ date, clicks }) => ({
+      x: new Date(date),
+      y: clicks,
+    }));
+    if (statsXY.length > 0) {
+      if (!datesAreOnSameDay(statsXY[0].x, startDate))
+        statsXY.unshift({ x: startDate, y: 0 });
+      if (!datesAreOnSameDay(statsXY[statsXY.length - 1].x, startDate))
+        statsXY.push({ x: endDate, y: 0 });
+    } else {
+      statsXY.push({ x: startDate, y: 0 });
+      statsXY.push({ x: endDate, y: 0 });
+    }
+    return statsXY;
+  }, [userStats, startDate, endDate]);
+  const viewsStatsXY = useCallback(() => {
+    const statsXY = userStats.map(({ date, page_views }) => ({
+      x: new Date(date),
+      y: page_views,
+    }));
+    if (statsXY.length > 0) {
+      if (!datesAreOnSameDay(statsXY[0].x, startDate))
+        statsXY.unshift({ x: startDate, y: 0 });
+      if (!datesAreOnSameDay(statsXY[statsXY.length - 1].x, startDate))
+        statsXY.push({ x: endDate, y: 0 });
+    } else {
+      statsXY.push({ x: startDate, y: 0 });
+      statsXY.push({ x: endDate, y: 0 });
+    }
+    return statsXY;
+  }, [userStats, startDate, endDate]);
+
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(
